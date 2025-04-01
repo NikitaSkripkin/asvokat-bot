8c47d7df26427402c61c29ffad934788e8f77cb5
NikitaSkripkin <skripkin.nikita.a@gmail.com>
2025-04-01 22:49:26 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "7657366517:AAH6bzYPe8Ls0vPAAOKAhBpNZfZbNuM0AuA"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        ["Уголовное право", "Конституция"],
+        ["Процессуальные права", "Контакты адвоката"]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    await update.message.reply_text(
+        "Добро пожаловать в юридического бота!\n\nВыберите интересующую вас тему:",
+        reply_markup=reply_markup
+    )
+
+if name == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
