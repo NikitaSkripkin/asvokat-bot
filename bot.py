e00b3a211ddab695b4575ec6e9f00f6ec20419e4
NikitaSkripkin <skripkin.nikita.a@gmail.com>
2025-04-01 23:15:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,17 @@
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "7657366517:AAH6bzYPe8Ls0vPAAOKAhBpNZfZbNuM0AuA"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
+async def запуск(update, context: ContextTypes.DEFAULT_TYPE):
+    клавиатура = [
         ["Уголовное право", "Конституция"],
-        ["Процессуальные права", "Контакты адвоката"]
+        ["Административные права", "Контакты адвоката"]
     ]
-    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+    reply_markup = ReplyKeyboardMarkup(клавиатура, resize_keyboard=True)
+
     await update.message.reply_text(
-        "Добро пожаловать в юридического бота!\n\nВыберите интересующую вас тему:",
+        "Добро пожаловать в юридический бот!\n\nВыберите интересующую вас тему:",
         reply_markup=reply_markup
     )
-
-if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
